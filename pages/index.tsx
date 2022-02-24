63b39f977b61cd69ca4fd669e72685ce25243e7b
Evan Walter <evan.walter.software@gmail.com>
2022-02-23 17:09:16 -0800

    Set up project with Typescript

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+
+  const message: string = 'Hello, world!'
+  
+  return (
+    <div>
+      {message}
+    </div>
+  )
+}
