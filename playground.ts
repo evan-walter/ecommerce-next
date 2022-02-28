2faf378eada83ed22dd81da8ba7a70b1dd3398e0
Evan Walter <evan.walter.software@gmail.com>
2022-02-28 15:16:46 -0800

    Demonstrating ts error declaring parameters of any type

--- playground.ts
+++ playground.ts
@@ -1,3 +1,15 @@
-export default function play () {
-  console.log('Hello, world!')
+export default function play() {
+  const name = 'Evan'
+  const age = 29
+
+  function logPersonInfo(personName, personAge) {
+    const info = `Name: ${personName}, Age: ${personAge}`
+    console.log(info)
+    return info
+  }
+
+  logPersonInfo(name, age)
+  logPersonInfo(true, false)
+  logPersonInfo(12, 12)
+  logPersonInfo(null, undefined)
 }
