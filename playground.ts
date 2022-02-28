3413036cd52ab2bbf100a43df8136ce07c167ebd
Evan Walter <evan.walter.software@gmail.com>
2022-02-28 15:23:01 -0800

    Declare parameter types (fixed)

--- playground.ts
+++ playground.ts
@@ -2,7 +2,7 @@ export default function play() {
   const name = 'Evan'
   const age = 29
 
-  function logPersonInfo(personName: string, personAge: ) {
+  function logPersonInfo(personName: string, personAge: number) {
     const info = `Name: ${personName}, Age: ${personAge}`
     console.log(info)
     return info
