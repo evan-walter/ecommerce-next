2c238ee218c30833421f4f14dc1287933449b286
Evan Walter <evan.walter.software@gmail.com>
2022-02-28 15:27:26 -0800

    Object types

--- playground.ts
+++ playground.ts
@@ -2,11 +2,23 @@ export default function play() {
   const name = 'Evan'
   const age = 29
 
+  const person = {
+    name: 'John',
+    age: 34,
+  }
+
   function logPersonInfo(personName: string, personAge: number) {
     const info = `Name: ${personName}, Age: ${personAge}`
     console.log(info)
     return info
   }
 
+  function logPersonInfo2(person: { name: string; age: number }) {
+    const info = `Name: ${person.name}, Age: ${person.age}`
+    console.log(info)
+    return info
+  }
+
   logPersonInfo(name, age)
+  logPersonInfo2(person)
 }
