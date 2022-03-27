ae950cd3894e1ef9571882c695a3fb1ed0d3e9a8
Evan Walter <evan.walter.software@gmail.com>
2022-03-27 14:45:59 -0700

    define arrays

--- playground.ts
+++ playground.ts
@@ -1,53 +1,9 @@
 
-// interface Person {
-//   name: string
-//   age: number
-// }
-
-class Person {
-  name: string
-  age?: number
-  
-  constructor(name: string, age: number) {
-    this.name = name
-    this.age = age
-  }
-}
-
-// interface PersonLoggerFn {
-//   (name: string, age: number): string
-// }
-
-type PersonLoggerFn = (name: string, age?: number) => string
-
 
 export default function play() {
-  const name = 'Evan'
-  const age = 29
+// Two different means to define arrays
+const names: string[] = ['Deer', 'John']
+const numbers: Array<number> = [1, 2, 3, 4, 5]
 
-  const john: Person = {
-    name: 'John',
-  }
-
-
-  const logPersonInfo: PersonLoggerFn = (
-    personName: string, personAge: number = 0
-  ): string => {
-    const info = `Name: ${personName}, Age: ${personAge}`
-    console.log(info)
-    return info
-  }
-
-  function logPersonInfo2(person: Person) {
-    // const info = 'Name: ' + personName + ', age: ' + personAge
-    const info = `Name: ${person.name}, Age: ${person.age}`
-    console.log(info)
-    return info
-  }
-
-  const log = logPersonInfo(name)
-  
-  const person = new Person('Edward', 50)
-
-  logPersonInfo2(person)
+  const random = Math.random() > 0.5 ? 'hello' : [1, 2]
 }
