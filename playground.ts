677a071a621a43614530ffeafcc30ca2ec30666b
Evan Walter <evan.walter.software@gmail.com>
2022-03-27 14:46:50 -0700

    define arrays (with proper indentation)

--- playground.ts
+++ playground.ts
@@ -1,9 +1,9 @@
 
 
 export default function play() {
-// Two different means to define arrays
-const names: string[] = ['Deer', 'John']
-const numbers: Array<number> = [1, 2, 3, 4, 5]
+  // Two different means to define arrays
+  const names: string[] = ['Deer', 'John']
+  const numbers: Array<number> = [1, 2, 3, 4, 5]
 
   const random = Math.random() > 0.5 ? 'hello' : [1, 2]
 }
