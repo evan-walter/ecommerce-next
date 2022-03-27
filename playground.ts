adf92cdbf957dac3e46485b9d48ccd4aa60c9b22
Evan Walter <evan.walter.software@gmail.com>
2022-03-27 14:51:19 -0700

    narrowing

--- playground.ts
+++ playground.ts
@@ -1,9 +1,14 @@
 
 
 export default function play() {
-  // Two different means to define arrays
-  const names: string[] = ['Deer', 'John']
-  const numbers: Array<number> = [1, 2, 3, 4, 5]
-
   const random = Math.random() > 0.5 ? 'hello' : [1, 2]
+
+  if (typeof random === 'string') {
+    const upper = random.toUpperCase()
+  } else {
+    console.log(random);
+  }
+
+  console.log(random.length);
+
 }
