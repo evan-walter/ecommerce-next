0e7b56130067d1ddcab5b52a1628615b89124108
Evan Walter <evan.walter.software@gmail.com>
2022-10-26 15:54:13 -0700

    infer get static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect } from 'react'
-import play from '../playground'
-import styles from '../styles/Home.module.css'
+import type { InferGetStaticPropsType } from 'next'
 
-export default function Home() {
+export async function getStaticProps() {
+  const products = [1, 2, 3]
 
-  useEffect(() => {
-    play()
-  }, [])
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  }
+}
 
-  return (
-    <div>
-      Hello, world!
-    </div>
-  )
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div>{products}</div>
 }
